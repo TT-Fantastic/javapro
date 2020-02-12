f1f0f3d9bc6ca80506dd12bad13a034091b5f7e2
TT-fantastic <648687176@qq.com>
2020-02-12 23:53:52 +0800

    test

--- src/com/neuedu/test/TestFTW.java
+++ src/com/neuedu/test/TestFTW.java
@@ -5,6 +5,19 @@ import java.io.*;
 
 public class TestFTW {
     public static void main(String[] args) {
+       MyLibrary myLibrary=new MyLibrary("你懂得",286.0,"你懂得","你懂得","你懂得");
+       OutputStream outputStream=null;
+       ObjectOutputStream objectOutputStream=null;
+       File file=new File("h:/");
+        try {
+            outputStream=new FileOutputStream(file);
+            objectOutputStream=new ObjectOutputStream(outputStream);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
